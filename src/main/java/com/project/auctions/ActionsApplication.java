a13fdbbac312c6e070f093f21e48377d988c5656
D4nielZ4k <78098047+D4nielZ4k@users.noreply.github.com>
2021-08-31 21:05:19 +0200

    CORS

--- src/main/java/com/project/auctions/ActionsApplication.java
+++ src/main/java/com/project/auctions/ActionsApplication.java
@@ -2,12 +2,26 @@ package com.project.auctions;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ActionsApplication {
+@EnableGlobalMethodSecurity(
+        prePostEnabled = true,
+        securedEnabled = true,
+        jsr250Enabled = true
+)
+public class ActionsApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(ActionsApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ActionsApplication.class);
     }
 
 }
